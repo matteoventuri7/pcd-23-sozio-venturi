d70d13b4e2d3757ac4291e1481f61eb4238584ad
diego sozio <diego.sozio92@gmail.com>
2023-07-26 20:13:00 +0200

    added at Main.java multiple appoch

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,55 @@
-import java.io.IOException;
 import java.nio.file.Path;
-import java.util.Arrays;
+
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        IWordSearcher s = new MultiThreadFileSearcher(Path.of("C://test"), "ciao");
+        String defaultFolderPath = "/Users/diegosozio/Documents/PCD/test";
+        String defaultKeyword = "ciao";
+
+        if (args.length == 0) {
+            System.out.println("Usage: java Main <approach> ");
+            System.out.println("Available approaches:");
+            System.out.println("1 - MultiThreadFileSearcher");
+            System.out.println("2 - VirtualThreadFileSearcher");
+            System.out.println("3 - TaskFileSearcher");
+            return;
+        }
+
+        // Utilizza i parametri della riga di comando per scegliere l'approccio
+        String selectedApproach = args[0].toLowerCase();
+
+        // Verifica se l'approccio selezionato è valido
+        switch (selectedApproach) {
+            case "1":
+                break;
+            case "2":
+                break;
+            case "3":
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid approach selected. Please choose an appropriate approach.");
+        }
+
+        // Utilizza i parametri della riga di comando se forniti, altrimenti utilizza quelli di default
+        String folderPath = args.length > 1 ? args[1] : defaultFolderPath;
+        String keyword = args.length > 2 ? args[2] : defaultKeyword;
+
+        IWordSearcher s = null;
+
+        // Scegli l'approccio in base al parametro selezionato
+        switch (selectedApproach) {
+            case "1":
+                s = new MultiThreadFileSearcher(Path.of(folderPath), keyword);
+                break;
+            case "2":
+                s = new VirtualThreadFileSearcher(Path.of(folderPath), keyword);
+                break;
+            case "3":
+                s = new TaskFileSearcher(Path.of(folderPath), keyword);
+                break;
+        }
+
+        // Esegui l'approccio selezionato
         s.search();
         Thread.sleep(10);
         s.pause();
@@ -20,8 +65,7 @@ public class Main {
         System.out.println("IsPartial:" + result.isPartial());
         System.out.println("Total files:" + result.getTotalFiles());
         System.out.println("Found files:" + result.getFiles().size());
-        for (Path file :
-                result.getFiles()) {
+        for (Path file : result.getFiles()) {
             System.out.println(file.toString());
         }
     }
