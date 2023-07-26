d18ae741afa5a04dff8ad144407ef73e94fb2fd3
diego sozio <diego.sozio92@gmail.com>
2023-07-26 19:16:00 +0200

    added computing time label + counter

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -13,14 +13,16 @@ public class MainGUI {
     private static JLabel totalFilesLabel;
     private static JLabel foundPdfFilesLabel;
     private static JLabel pdfFilesWithKeywordLabel;
+    private static JLabel computingTimeLabel; // New label for computing time
     private static JButton stopButton;
     private static JButton startButton;
-    private static String keyword; // Variable to store the searched keyword
+    private static String keyword;
     private static volatile boolean isStopping = false;
     private static volatile boolean isSuspended = false;
-
-    // Dichiara la JComboBox
     private static JComboBox<String> emptySelect;
+
+    private static Timer computingTimer; // Timer to measure computing time
+    private static long startTime; // Variable to store the start time
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("File Search GUI");
@@ -61,8 +63,8 @@ public class MainGUI {
                     updateOutputAreaThread = null;
                 }
             }
-
         });
+
         frame.setSize(800, 800);
         frame.setLayout(new BorderLayout());
 
@@ -83,7 +85,7 @@ public class MainGUI {
         outputArea.setEditable(false);
         JScrollPane scrollPane = new JScrollPane(outputArea);
 
-        // Inizializza la JComboBox
+        // Initialize the JComboBox
         emptySelect = new JComboBox<>();
 
         inputPanel.add(new JLabel("Folder:"));
@@ -98,23 +100,29 @@ public class MainGUI {
         inputPanel.add(foundPdfFilesLabel);
         inputPanel.add(pdfFilesWithKeywordLabel);
 
-        // Aggiungi la JComboBox al pannello di input
+        // Add the JComboBox to the input panel
         inputPanel.add(new JLabel("Select:"));
         inputPanel.add(emptySelect);
 
         frame.add(inputPanel, BorderLayout.NORTH);
         frame.add(scrollPane, BorderLayout.CENTER);
 
-        // Aggiungi gli elementi alla JComboBox
+        // Add elements to the JComboBox
         emptySelect.addItem("Option 1");
         emptySelect.addItem("Option 2");
         emptySelect.addItem("Option 3");
 
+        // Create the computing time label
+        computingTimeLabel = new JLabel("Computing Time: ");
+        inputPanel.add(computingTimeLabel);
+
         startButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                // Store the start time when the "Start" button is pressed
                 String folderPath = folderField.getText();
                 keyword = keywordField.getText().trim(); // Save the searched keyword
+                startTime = System.nanoTime();
                 stopButton.setEnabled(true);
                 startButton.setEnabled(false);
                 System.out.println("STARTED");
@@ -129,15 +137,18 @@ public class MainGUI {
                     s = new MultiThreadFileSearcher(Path.of(folderPath), keyword);
                     s.search();
 
-                    // La ricerca è terminata, chiamiamo il metodo per segnalarlo
+                    // The search has finished, call the method to signal it
                     searchFinished();
                 } catch (Exception ex) {
                     outputArea.append("Error: " + ex.getMessage() + "\n");
-                    // Assicuriamoci comunque di segnalare la fine della ricerca in caso di errore
+                    // Still make sure to signal the end of the search in case of error
                     searchFinished();
                 }
             }
         });
+
+        frame.setVisible(true);
+        updateOutputArea();
 
         stopButton.addActionListener(new ActionListener() {
             @Override
@@ -171,7 +182,7 @@ public class MainGUI {
             public void actionPerformed(ActionEvent e) {
                 if (s != null) {
                     s.pause();
-                    isSuspended = true; // Imposta il flag per indicare che la ricerca è sospesa
+                    isSuspended = true; // Set the flag to indicate the search is suspended
                     System.out.println("SUSPENDED");
                     stopButton.setEnabled(false);
                     suspendButton.setEnabled(false);
@@ -185,18 +196,18 @@ public class MainGUI {
             public void actionPerformed(ActionEvent e) {
                 if (s != null) {
                     s.pause();
-                    isSuspended = false; // Ripristina il flag per indicare che la ricerca non è più sospesa
+                    isSuspended = false; // Reset the flag to indicate the search is no longer suspended
                     System.out.println("RESUMED");
                     stopButton.setEnabled(true);
                     suspendButton.setEnabled(true);
                     resumeButton.setEnabled(false);
 
-                    // Rimuovi il testo dell'area di output quando si preme "Resume"
+                    // Remove the output area text when "Resume" is pressed
                     outputArea.setText("");
                     totalFilesLabel.setText("Total files: 0");
                     foundPdfFilesLabel.setText("Found PDF files: 0");
                     pdfFilesWithKeywordLabel.setText("PDF files with keyword: 0");
-                    keyword = keywordField.getText().trim(); // Aggiorna la parola chiave cercata
+                    keyword = keywordField.getText().trim(); // Update the searched keyword
                 }
             }
         });
@@ -219,7 +230,7 @@ public class MainGUI {
                         try {
                             Thread.sleep(100); // Add a short delay to avoid busy-waiting
                         } catch (InterruptedException e) {
-                            // Va bene
+                            // It's okay
                         }
                     }
 
@@ -251,9 +262,9 @@ public class MainGUI {
                     }
 
                     try {
-                        Thread.sleep(100); // Aggiungi un breve ritardo per evitare busy-waiting
+                        Thread.sleep(100); // Add a short delay to avoid busy-waiting
                     } catch (InterruptedException e) {
-                        // Va bene
+                        // It's okay
                     }
                 }
             }
@@ -261,16 +272,22 @@ public class MainGUI {
         updateOutputAreaThread.setDaemon(true);
         updateOutputAreaThread.start();
     }
-    // Metodo per segnalare la fine della ricerca
+
+    // Method to signal the end of the search
     private static void searchFinished() {
         if (updateOutputAreaThread != null) {
-            // Riavviamo il thread di aggiornamento se è stato interrotto
+            // Restart the update thread if it was interrupted
             if (updateOutputAreaThread.isInterrupted()) {
                 updateOutputAreaThread.interrupt();
             }
-            // Reset dei flag per indicare che la ricerca è terminata e può essere ripresa nuovamente
+            // Reset the flags to indicate that the search has ended and can be resumed again
             isStopping = false;
             isSuspended = false;
+            // Compute and update the computing time label
+            long endTime = System.nanoTime();
+            long computingTimeNano = endTime - startTime;
+            double computingTimeMillis = computingTimeNano / 1_000_000.0;
+            computingTimeLabel.setText("Computing Time: " + computingTimeMillis + " ms");
         }
     }
 }
