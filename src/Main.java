8e22e274c41d1d526340b75bfed519874495e550
diego sozio <diego.sozio92@gmail.com>
2023-08-03 10:43:40 +0200

    update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,74 +1,98 @@
+// per mac  var s = new MultiThreadFileSearcher(Path.of("/Users/diegosozio/Documents/PCD/test"), "ciao");
+// per windows  var s = new MultiThreadFileSearcher(Path.of("C://test"), "ciao");*/
+import java.io.IOException;
 import java.nio.file.Path;
-
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        String defaultFolderPath = "/Users/diegosozio/Documents/PCD/test";
-        String defaultKeyword = "ciao";
 
-        if (args.length == 0) {
-            System.out.println("Usage: java Main <approach> ");
-            System.out.println("Available approaches:");
-            System.out.println("1 - MultiThreadFileSearcher");
-            System.out.println("2 - VirtualThreadFileSearcher");
-            System.out.println("3 - TaskFileSearcher");
-            return;
-        }
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
 
-        // Utilizza i parametri della riga di comando per scegliere l'approccio
-        String selectedApproach = args[0].toLowerCase();
+        String folderPath;
+        String keyword;
 
-        // Verifica se l'approccio selezionato è valido
-        switch (selectedApproach) {
-            case "1":
-                break;
-            case "2":
-                break;
-            case "3":
-                break;
-            default:
-                throw new IllegalArgumentException("Invalid approach selected. Please choose an appropriate approach.");
-        }
+        // Richiedi all'utente di inserire il percorso della cartella
+        System.out.println("Enter the folder path:");
+        folderPath = scanner.nextLine();
 
-        // Utilizza i parametri della riga di comando se forniti, altrimenti utilizza quelli di default
-        String folderPath = args.length > 1 ? args[1] : defaultFolderPath;
-        String keyword = args.length > 2 ? args[2] : defaultKeyword;
+        // Richiedi all'utente di inserire la parola da cercare
+        System.out.println("Enter the keyword:");
+        keyword = scanner.nextLine();
 
-        IWordSearcher s = null;
+        System.out.println("Select the approach (1: Multithreaded, 2: Virtual Thread, 3: Task Java):");
+        int approachChoice = scanner.nextInt();
+        scanner.nextLine(); // Consume the remaining newline character
 
-        // Scegli l'approccio in base al parametro selezionato
-        switch (selectedApproach) {
-            case "1":
+        final IWordSearcher s;
+        switch (approachChoice) {
+            case 1:
                 s = new MultiThreadFileSearcher(Path.of(folderPath), keyword);
                 break;
-            case "2":
+            case 2:
                 s = new VirtualThreadFileSearcher(Path.of(folderPath), keyword);
                 break;
-            case "3":
-                //s = new TaskFileSearcher(Path.of(folderPath), keyword);
+            case 3:
+                // Implement the Task Java approach here if needed
+                s = null;
                 break;
+            default:
+                System.out.println("Invalid approach choice. Exiting...");
+                return;
         }
 
-        // Esegui l'approccio selezionato
-        s.start();
-        Thread.sleep(10);
-        s.pause();
-        System.out.println("MAIN-pause");
-        Thread.sleep(3000);
-        s.resume();
-        System.out.println("MAIN-resume");
-        s.close();
-        PrintResult(s.getResult());
+        if (s != null) {
+            s.register(new IGuiRegistrable() {
+                @Override
+                public void onNewResultFile(ResultEventArgs ev) {
+                    System.out.println("Found: " + ev.getFile().toString());
+                }
+
+                @Override
+                public void onNewFoundFile(long totalFiles) {
+                    System.out.println("Total files processed: " + totalFiles);
+                }
+
+                @Override
+                public void onFinish(SearchResult result) {
+                    System.out.println("Total files found: " + result.getFiles().size());
+                    System.out.println("Total files processed: " + result.getTotalFiles());
+                    System.out.println("Computing Time: " + s.getElapsedTime() + " ms");
+                }
+            });
+
+            System.out.println("Starting the search...");
+            s.start();
+
+            // Pause after 10 milliseconds
+            sleep(10);
+
+            System.out.println("Pausing the search...");
+            s.pause();
+
+            // Resume after 3000 milliseconds (3 seconds)
+            sleep(3000);
+
+            System.out.println("Resuming the search...");
+            s.resume();
+
+            System.out.println("Closing the search...");
+            try {
+                s.close();
+            } catch (java.io.IOException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
-    private static void PrintResult(SearchResult result) {
-        System.out.println("Total files:" + result.getTotalFiles());
-        System.out.println("Found files:" + result.getFiles().size());
-        for (Path file : result.getFiles()) {
-            System.out.println(file.toString());
+    private static void sleep(int milliseconds) {
+        try {
+            Thread.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
 
-// per mac  var s = new MultiThreadFileSearcher(Path.of("/Users/diegosozio/Documents/PCD/test"), "ciao");
-// per windows  var s = new MultiThreadFileSearcher(Path.of("C://test"), "ciao");
