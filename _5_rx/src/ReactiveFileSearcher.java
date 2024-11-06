07260df73752ed7fbc3b9e34c9859da60480f9ff
Matteo V <matteo.venturi7@gmail.com>
2024-11-06 01:43:23 +0100

    refactored approach

--- _5_rx/src/ReactiveFileSearcher.java
+++ _5_rx/src/ReactiveFileSearcher.java
@@ -3,14 +3,10 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
 import io.reactivex.rxjava3.subjects.PublishSubject;
 import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.concurrent.Semaphore;
 
 public class ReactiveFileSearcher extends AFilePDFSearcher {
-    private int nComputedFiles=0;
     private PublishSubject<Path> sourcePaths;
     private Disposable toDispose;
-    private Semaphore finishSem = new Semaphore(1);
-    private boolean finishAlreadyNotified = false;
 
     public ReactiveFileSearcher(Path start, String word) {
         super(start, word);
@@ -18,27 +14,17 @@ public class ReactiveFileSearcher extends AFilePDFSearcher {
 
     @Override
     public void start() {
-        nComputedFiles=0;
-
         if(sourcePaths == null) {
             sourcePaths = PublishSubject.<Path>create();
 
             toDispose = sourcePaths
                 .subscribeOn(Schedulers.computation())
-                .observeOn(Schedulers.single()) // to decouple search file to computation
                 .subscribe(file -> {
                     var isPositive = AFilePDFSearcher.searchWordInPDF(file, word);
                     if(isPositive) {
                         AddResultAndNotify(file);
-                        try {
-                            finishSem.acquire();
-                            notifyIfFinished();
-                            finishSem.release();
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
                     }
-                });
+                }, Throwable::printStackTrace, this::notifyFinish);
         }
 
         super.start();
@@ -52,31 +38,13 @@ public class ReactiveFileSearcher extends AFilePDFSearcher {
             throw new RuntimeException(e);
         }
 
-        nComputedFiles++;
         sourcePaths.onNext(file);
-    }
-
-    protected void notifyIfFinished() {
-        if (!finishAlreadyNotified && isResearchFinished() && isFinished()) {
-            finishAlreadyNotified = true;
-            notifyFinish();
-        }
-    }
-
-    private boolean isFinished() {
-        return !isPaused() && nComputedFiles == getResult().getTotalFiles();
     }
 
     @Override
     protected void onSearchIsFinished() {
         super.onSearchIsFinished();
-        try {
-            finishSem.acquire();
-            notifyIfFinished();
-            finishSem.release();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        sourcePaths.onComplete();
     }
 
     @Override
